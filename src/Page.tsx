89eca01a8a3eb1695e0b0b1b92af85d9989b7292
Jaime Pericas <jaime.pericas.saez@gmail.com>
2024-08-08 00:13:51 -0700

    handle portrait mode

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,7 +1,38 @@
 import "./Page.css"
+import ScreenLockLandscapeIcon from "@mui/icons-material/ScreenLockLandscape"
+import StayCurrentLandscapeIcon from "@mui/icons-material/StayCurrentLandscape"
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward"
 
 export const Page: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
-  return <div className="page-container">{children}</div>
+  return (
+    <div className="page-container">
+      <div className="page-container-landscape">
+        <p>
+          This app does not work in landscape mode. To continue, please orient
+          your device to portrait mode.
+        </p>
+        <div className="page-container-landscape-icons">
+          <ScreenLockLandscapeIcon
+            style={{
+              fontSize: "5rem",
+            }}
+          />
+          <ArrowForwardIcon
+            style={{
+              fontSize: "5rem",
+            }}
+          />
+          <StayCurrentLandscapeIcon
+            style={{
+              fontSize: "5rem",
+              transform: "rotate(90deg)",
+            }}
+          />
+        </div>
+      </div>
+      {children}
+    </div>
+  )
 }
